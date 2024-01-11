053703f0282ec8f1b92a077b18949a5a11cd1540
lisalazim <lisalazi21@gmail.com>
2024-01-11 20:09:30 +0700

    Add project files.

---
+++ WindowsFormsApp1/Form1.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics.Eventing.Reader;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label4_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label5_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void logIn_Click(object sender, EventArgs e)
+        {
+            if (username.Text == "lalalisa" &&  password.Text == "4444")
+            {
+                new Form2().Show();
+                this.Hide();
+            }
+                
+            else
+            { 
+                MessageBox.Show("Oops! your username or your password is incorrect.");
+                username.ResetText();
+                password.ResetText();
+                username.Focus();
+            
+            }
+        }
+
+        private void label9_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label3_Click_1(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
